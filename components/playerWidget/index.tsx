d23e7e1f57910a8fe72165e54f4efae034f75da6
Matias Safranchik <msafranchik@TEC32M>
2020-10-26 17:24:45 -0300

    add widget functionality

--- components/playerWidget/index.tsx
+++ components/playerWidget/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, Image, View, TouchableOpacity } from 'react-native';
 import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import { Sound } from 'expo-av/build/Audio/Sound';
 
 import styles from './styles';
 
 const song = {
   id: '1',
+  uri: '',
   imageUri:
     'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
   title: 'High on You',
@@ -13,8 +15,63 @@ const song = {
 };
 
 export default () => {
+  const [sound, setSound] = useState<Sound | null>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(true);
+  const [duration, setDuration] = useState<number | null>(null);
+  const [position, setPosition] = useState<number | null>(null);
+
+  const onPlaybackStatusUpdate = (status) => {
+    setIsPlaying(status.isPlaying);
+    setDuration(status.durationMillis);
+    setPosition(status.positionMillis);
+  };
+
+  const playCurrentSong = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    const { sound: newSound } = await Sound.createAsync(
+      { uri: song.uri },
+      { shouldPlay: isPlaying },
+      onPlaybackStatusUpdate
+    );
+
+    setSound(newSound);
+  };
+
+  useEffect(() => {
+    if (song) {
+      playCurrentSong();
+    }
+  }, [song]);
+
+  const onPlayPausePress = async () => {
+    if (!sound) {
+      return;
+    }
+    if (isPlaying) {
+      await sound.stopAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
+
+  const getProgress = () => {
+    if (sound === null || duration === null || position === null) {
+      return 0;
+    }
+
+    return (position / duration) * 100;
+  };
+
+  if (!song) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
+      <View style={[styles.progress, { width: `${getProgress()}%` }]} />
       <View style={styles.row}>
         <Image source={{ uri: song.imageUri }} style={styles.image} />
         <View style={styles.rightContainer}>
@@ -25,8 +82,12 @@ export default () => {
 
           <View style={styles.iconsContainer}>
             <AntDesign name='hearto' size={30} color={'white'} />
-            <TouchableOpacity>
-              <FontAwesome name={'play'} size={30} color={'white'} />
+            <TouchableOpacity onPress={onPlayPausePress}>
+              <FontAwesome
+                name={isPlaying ? 'pause' : 'play'}
+                size={30}
+                color={'white'}
+              />
             </TouchableOpacity>
           </View>
         </View>
