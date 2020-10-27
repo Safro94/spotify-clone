db1803bf7df1e4c5aeccbfb61586a6c49d1e13bd
Matias Safranchik <msafranchik@TEC32M>
2020-10-27 10:30:31 -0300

    setup folder structure

---
+++ server/src/api/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const songs = require('./routes/songs');
+const errorHandler = require('../middlewares/errorHandler');
+const notFound = require('../middlewares/notFound');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/songs', songs);
+
+// Not found
+app.use(notFound);
+
+// Error handler
+app.use(errorHandler);
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server running on port ${port}`));
